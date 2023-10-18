ec944926e2b1cb74b2d24239a96974836ac346c3
Vikash Sharma <Vikash9422@gmail.com>
2023-10-18 22:22:48 +0530

    02_DataType.js Created

---
+++ 01_Basic/02_DataType.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS Code as newer version
+
+let name = "vikash"; // String datatype
+let age = 21; // number datatype
+let isLogged = false; // boolean datatype
+
+// number => 2 to power 53 (limit)
+// bigint   upto number limit
+// String => ""
+// boolean => yes or no (true or false)
+// null => standalone value
+// undefined =>
+// symbole => used for uniqueness
+
+// Object
+
+console.log(typeof name);
+console.log(typeof undefined); // it gives undefined
+console.log(typeof null); // it give object in console
